d7f85c18a4319f11fe43f5aaf4fc1b0fd637533f
MadImp <60294415+Mad-Imp@users.noreply.github.com>
2021-05-05 17:45:25 +0300

    basic layout Header

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -3,19 +3,19 @@ import logo from "../../assets/logo.png";
 import Button from "../button/Button";
 
 function Contacts() {
- return (
-     <>
-         <div className={styles.wrapper}>
-             <div className={styles.contacts}>
-                 <img src={logo} alt="лого" className={styles.logo}/>
-                 <p>
-                     <a className={styles.phone}href="tel:+74742476245">+7(4742) 47-62-45</a>
-                 </p>
-                 <Button/>
-             </div>
-         </div>
-     </>
- )
+    return (
+        <>
+            <div className={styles.wrapper}>
+                <div className={styles.contacts}>
+                    <img src={logo} alt="лого" className={styles.logo}/>
+                    <div className={styles.wrap}>
+                        <a className={styles.phone} href="tel:+74742476245">+7(4742) 47-62-45</a>
+                        <Button/>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Contacts;
