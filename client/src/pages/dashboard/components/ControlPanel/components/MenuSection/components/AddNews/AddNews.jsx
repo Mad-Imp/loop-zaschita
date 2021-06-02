107819b899766e8affc272e25ee6f23a1855b554
Vadim Obanin <mortido3@mail.ru>
2021-06-02 08:30:49 +0300

    Start of text editor development.

--- client/src/pages/dashboard/components/ControlPanel/components/MenuSection/components/AddNews/AddNews.jsx
+++ client/src/pages/dashboard/components/ControlPanel/components/MenuSection/components/AddNews/AddNews.jsx
@@ -1,6 +1,28 @@
+import styles from './AddNews.module.scss'
+
 function AddNews() {
+
+  const upload = (e) => {
+    const file = e.target.files[0]
+    fetch('/saveImage', {
+      method: 'POST',
+      body: file
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
   return (
-    <h1>AddNews</h1>
+    <div className={styles.wrap}>
+      <h1 contentEditable>Hello</h1>
+      <p contentEditable>dflskdjflsjdlfsjldfk</p>
+      <input name='myimage' onChange={upload} type="file"/>
+    </div>
   )
 }
 
