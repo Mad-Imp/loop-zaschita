bfbdeabe2c77bde6ec5602b6a27d6c8553e8745d
MadImp <60294415+Mad-Imp@users.noreply.github.com>
2021-06-12 12:48:00 +0300

    add ActivityList to services page, modified about page, modified news page

--- client/src/pages/news/News.jsx
+++ client/src/pages/news/News.jsx
@@ -5,6 +5,7 @@ import {useEffect, useState} from "react";
 import NewsItem from "./components/newsItem/NewsItem";
 import NewsContent from "./components/newsContent/NewsContent";
 import Cards from '../../components/cards/Cards';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 
 function News() {
@@ -19,8 +20,6 @@ function News() {
             .then(data => {
                 setNews(data.reverse())
             })
-        console.log('UseEffect')
-
     }, [count])
 
     const incCount = () => {
@@ -30,17 +29,20 @@ function News() {
     return (
         <div className={styles.wrap}>
             <h1 className={styles.title}>Новости</h1>
-            <div className={styles.container}>
-                <Switch>
-                    <Route exact path="/news">
-                        {news.map(newsItem => (
-                            <NewsItem news={newsItem} key={newsItem.id}/>
-                        ))}
-                        <div className={styles.cards}><Cards/></div>
-                    </Route>
-                    <Route exact path="/news/:id"><NewsContent incCount={incCount} news={news}/></Route>
-                </Switch>
-            </div>
+            {news.length === 0
+                ? <LinearProgress/>
+                : <div className={styles.container}>
+                    <Switch>
+                        <Route exact path="/news">
+                            {news.map(newsItem => (
+                                <NewsItem news={newsItem} key={newsItem.id}/>
+                            ))}
+                            <div className={styles.cards}><Cards/></div>
+                        </Route>
+                        <Route exact path="/news/:id"><NewsContent incCount={incCount} news={news}/></Route>
+                    </Switch>
+                </div>
+            }
         </div>
     )
 }
--- client/src/pages/news/components/newsContent/NewsContent.jsx
+++ client/src/pages/news/components/newsContent/NewsContent.jsx
@@ -8,62 +8,64 @@ import {useState} from 'react';
 import {AddNews} from '../../../dashboard/components/ControlPanel/components/MenuSection/components/AddNews/AddNews';
 
 export default function NewsContent({news, incCount}) {
-  const {id} = useParams();
-  const [showEditWindow, setShowEditWindow] = useState(false)
+    const {id} = useParams();
+    const [showEditWindow, setShowEditWindow] = useState(false)
 
-  const editNews = () => {
-    setShowEditWindow(!showEditWindow)
-  }
-
-  const deleteNews = (id) => {
-    let item = {
-      id: id
+    const editNews = () => {
+        setShowEditWindow(!showEditWindow)
     }
-    fetch('/api/delete', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      body: JSON.stringify(item)
-    })
-      .then((response) => {
-        return response.json()
-      })
-      .then((data) => {
-      })
-      .then(
-        incCount()
-      )
-  }
 
+    const deleteNews = (id) => {
+        let item = {
+            id: id
+        }
+        fetch('/api/delete', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(item)
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+            })
+            .then(
+                incCount()
+            )
+    }
 
-return <div>
-  {news.filter((item) => item.id === +id).map((item, index) => (
-    <div className={styles.wrap} key={index}>
+    return <div>
+        {news.filter((item) => item.id === +id).map((item, index) => (
+            <div className={styles.wrap} key={index}>
                 <span onClick={() => incCount()}><Link to='/news'>
                     Вернуться к новостям
                 </Link>
                 </span>
-      {showEditWindow ?
-          (<div className={styles.editNews}>
-          <AddNews count={incCount} show={setShowEditWindow} id={item.id} title={item.title} description={item.description} images={item.images}/>
-          </div>) :
-        null}
-      <div className={styles.el}>
-        <h1 className={styles.title}>{item.title}</h1>
-        <div className={styles.edit}>
-          <EditIcon onClick={() => editNews(item.id)} className={styles.icon}/>
-          <Link to='/news'>
-            <DeleteForeverIcon onClick={() => deleteNews(item.id)} className={styles.icon}/>
-          </Link>
-        </div>
-      </div>
-      <div className={styles.slider}>
-        <Slider images={item.images}/>
-      </div>
-      <p className={styles.description}>{item.description}</p>
-      <p className={styles.date}>{item.date}</p>
+                {showEditWindow ?
+                    (<div className={styles.editNews}>
+                        <AddNews count={incCount} show={setShowEditWindow} id={item.id} title={item.title}
+                                 description={item.description} images={item.images}/>
+                    </div>) :
+                    null}
+                <div className={styles.el}>
+                    <h1 className={styles.title}>{item.title}</h1>
+                    <div className={styles.edit}>
+                        <EditIcon onClick={() => editNews(item.id)} className={styles.icon}/>
+                        <Link to='/news'>
+                            <DeleteForeverIcon onClick={() => deleteNews(item.id)} className={styles.icon}/>
+                        </Link>
+                    </div>
+                </div>
+                {item.images.length > 1
+                    ? <div className={styles.slider}>
+                        <Slider images={item.images}/>
+                    </div>
+                    : null}
+                <p className={styles.description}>{item.description}</p>
+                <p className={styles.date}>{item.date}</p>
+            </div>
+        ))}
     </div>
-  ))}
-</div>
 }
